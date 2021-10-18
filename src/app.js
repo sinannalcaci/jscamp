3c36cecfa3ecd0c89abe429e4bf233367c2f5466
akdora19 <sinannalcaci26@gmail.com>
2021-10-19 00:02:32 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,32 @@
+console.log("merhaba kodlama.io")
+
+
+let dolarBugun=9.30
+let dolarDun=9.20
+
+{
+    let dolarDun= 9.10
+}
+
+console.log(dolarDun)
+
+
+const euroDun=11.2
+//euroDun= 11
+
+console.log(euroDun)
+
+
+let konutKredileri = ["konut kredisi","emlak konut kredisi","kamu konut kredisi","özel konut kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i <konutKredileri.length;i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+console.log("</ul>")
+
+
+
+console.log(konutKredileri)
+
